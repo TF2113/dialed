28165b80ea4d93de607e8aad243120f04b857f3c
TF2113 <148654475+TF2113@users.noreply.github.com>
2026-01-14 14:37:24 +0000

    Create Spring Boot boilerplate

---
+++ backend/src/main/java/com/tom/dialed/DialedApplication.java
@@ -0,0 +1,13 @@
+package com.tom.dialed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tom/dialed/DialedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.dialed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
